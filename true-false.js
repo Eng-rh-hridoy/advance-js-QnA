3a2937d0c5921ae92bc15031c22eb347e4d2690d
Md. Rakibul Hasan <rhridoy.cse@gmail.com>
2021-07-26 22:27:56 +0600

    truthy and falsy condition check

---
+++ true-false.js
@@ -0,0 +1,20 @@
+ // Falsy conditions :-
+//  age = 0
+//  age = ""
+//  age = undefined or const age;
+//  age = false
+//  age = NaN
+//  age = null
+
+// Truthy conditions :-
+// age = " "
+// age = "0"
+// age = []
+
+ const age = 4;
+ if(age){
+    console.log("condition is true");
+ }
+ else{
+    console.log("condition is false");
+ }
